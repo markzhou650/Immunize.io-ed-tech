891789f27f88e21cbceefbc9f34938381a902477
Brian <bxian@my.bcit.ca>
2022-10-11 14:53:36 -0700

    updated index to use container and navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ChatBox from "./components/ChatBox";
+// import ChatBox from "./components/ChatBox";
+import Container from "./components/Container";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ChatBox />
+    {/* <ChatBox /> */}
+    <Navbar />
+    <Container />
   </React.StrictMode>
 );
