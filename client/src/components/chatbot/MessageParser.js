5335c16571df3bc06a5837a859e029a5f79d7725
Chris M <cmok97@gmail.com>
2022-10-19 00:16:37 -0700

    removed repeated code from previous merge conflict

--- client/src/components/chatbot/MessageParser.js
+++ client/src/components/chatbot/MessageParser.js
@@ -18,24 +18,14 @@ class MessageParser {
         } else {
           // get mad
           this.actionProvider.wrongAnswer()
-
-      if (lowercase.includes(this.state.topic[this.state.questionNumber].Answer)) {
-        this.actionProvider.incrementQuestion()
-        // test
-        // console.log("Correct")
-      } else {
-        // get mad
-        this.actionProvider.wrongAnswer()
-        // test
-        // console.log("Incorrect")
-      }
       // if (this.state.askingQuestions === 0) {
       //   console.log('confirm')
       // }
       // return this.actionProvider
-    }
+        }
     if (lowercase.includes("hello")){
       return this.actionProvider.greet();
+      }
     }
   }
 }
