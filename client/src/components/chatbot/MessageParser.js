f1d0ce65fa74f2de1fd7e11591a07495d43c28fc
Chris M <cmok97@gmail.com>
2022-10-19 12:42:26 -0700

    added a check in messageparser for when all questions have been asked and lets user know the quiz has ended

--- client/src/components/chatbot/MessageParser.js
+++ client/src/components/chatbot/MessageParser.js
@@ -5,24 +5,28 @@ class MessageParser {
     }
   
     parse(message) {
-      console.log(message)
+      // console.log(message)
       const lowercase = message.toLowerCase();
       if (this.state.askingQuestions) {
         // If the user types continue after getting the question wrong, the quiz will continue
         if (lowercase.includes("continue")) {
           return this.actionProvider.incrementQuestion()
         }
-        
+        // If all of the questions have been asked, signal that the quiz has ended
+        if (this.state.questionNumber === this.state.topic.length - 1) {
+          this.actionProvider.correctAnswer()
+          this.actionProvider.endOfQuiz()
+        }
         if (lowercase.includes(this.state.topic[this.state.questionNumber].Answer)) {
           this.actionProvider.correctAnswer()
         } else {
           // get mad
           this.actionProvider.wrongAnswer()
+        }
       // if (this.state.askingQuestions === 0) {
       //   console.log('confirm')
       // }
       // return this.actionProvider
-        }
     if (lowercase.includes("hello")){
       return this.actionProvider.greet();
       }
