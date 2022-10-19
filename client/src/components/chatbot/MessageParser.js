629273d4285797e2d6074aa654db81ec2509a268
Chris M <cmok97@gmail.com>
2022-10-18 18:24:48 -0700

    added handlers to respond with messages to indicate if answer is correct/incorrect

--- client/src/components/chatbot/MessageParser.js
+++ client/src/components/chatbot/MessageParser.js
@@ -8,9 +8,13 @@ class MessageParser {
       console.log(message)
       const lowercase = message.toLowerCase();
       if (this.state.askingQuestions) {
-
+        // If the user types continue after getting the question wrong, the quiz will continue
+        if (lowercase.includes("continue")) {
+          return this.actionProvider.incrementQuestion()
+        }
+        
         if (lowercase.includes(this.state.topic[this.state.questionNumber].Answer)) {
-          this.actionProvider.incrementQuestion()
+          this.actionProvider.correctAnswer()
         } else {
           // get mad
           this.actionProvider.wrongAnswer()
@@ -21,6 +25,8 @@ class MessageParser {
       if (lowercase.includes("hello")){
         return this.actionProvider.greet();
       }
+
+      
     }
   }
   
