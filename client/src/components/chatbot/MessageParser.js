dba381697ece4d146476f964406ae58c18bfe68c
Chris M <cmok97@gmail.com>
2022-10-25 14:14:41 -0700

    removed unecessary comments and code

--- client/src/components/chatbot/MessageParser.js
+++ client/src/components/chatbot/MessageParser.js
@@ -37,38 +37,7 @@ class MessageParser {
             this.actionProvider.incrementQuestion()
           }
         }
-        
-      
-    //   // console.log(message)
-    //   const lowercase = message.toLowerCase();
-    //   if (this.state.askingQuestions) {
-    //     // If the user types continue after getting the question wrong, the quiz will continue
-    //     if (lowercase.includes("continue")) {
-    //       return this.actionProvider.incrementQuestion()
-    //     }
-    //     if (lowercase.includes("start")) {
-    //       this.actionProvider.askQuestion(this.state.topic[this.state.questionNumber].Question)
-    //     }
-    //     // If all of the questions have been asked, signal that the quiz has ended
-    //     if (this.state.questionNumber === this.state.topic.length - 1) {
-    //       this.actionProvider.correctAnswer()
-    //       this.actionProvider.endOfQuiz()
-    //     }
-    //     if (lowercase.includes(this.state.topic[this.state.questionNumber].Answer)) {
-    //       this.actionProvider.correctAnswer()
-    //     } else {
-    //       // get mad
-    //       this.actionProvider.wrongAnswer()
-    //     }
-    //   // if (this.state.askingQuestions === 0) {
-    //   //   console.log('confirm')
-    //   // }
-    //   // return this.actionProvider
-    // if (lowercase.includes("hello")){
-    //   return this.actionProvider.greet();
-    //   }
-    // }
-  }
+    }
 }
 
 export default MessageParser;
