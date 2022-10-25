819330ab9dd19957f6e500dc7be8464b69686679
your name <chanwoooolee@gmail.com>
2022-10-24 23:09:38 -0700

    created a sample iframe

---
+++ client/src/components/iframe/Iframe.js
@@ -0,0 +1,12 @@
+import '../styles/iframe.css'
+
+
+const IframeContainer = () => {
+    return (
+        <div className="iframe-wrapper">
+            <iframe src='https://school.moodledemo.net/login/index.php'></iframe>
+        </div>
+    )
+}
+
+export default IframeContainer
