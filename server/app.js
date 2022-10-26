c821425ca33003dccf5deaef5a5d309adbf0ca89
Brian Xian <brianxian2607@gmail.com>
2022-10-26 16:55:42 -0700

    connected api to database and added mysql and dotenv npm packages

--- server/app.js
+++ server/app.js
@@ -1,39 +1,61 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3001;
-const fs = require("fs");
-
-let data = fs.readFileSync("./JSONtests/mcqtest.json");
-let questions = JSON.parse(data);
+const mysql = require("mysql");
 
 app.use(express.json());
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  database: process.env.DB,
+  user: process.env.USER,
+  password: process.env.PWD,
+  insecureAuth: true,
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
 
 app.get("/questions", (req, res) => {
   // res.json({ message: "If you see this message, Node is connected" });
   // res.json({ message: questions[1].Question })
+  connection.query(
+    "SELECT * FROM questions",
+    function (error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+    }
+  );
   res.json({ questions: questions });
 });
 
-app.post("/questions", (req, res) => {
-  let key = Object.keys(req.body)[0];
-  let value = Object.values(req.body)[0];
-  questions[key] = value;
-  fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
-  res.json(questions);
-});
+// app.post("/questions", (req, res) => {
+//   let key = Object.keys(req.body)[0];
+//   let value = Object.values(req.body)[0];
+//   questions[key] = value;
+//   fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
+//   res.json(questions);
+// });
 
-app.put("/questions", (req, res) => {
-  let key = Object.keys(req.body)[0];
-  let value = Object.values(req.body)[0];
-  // if (key in questions) {
-  //   questions[key] = value;
-  //   fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
-  //   res.json(questions);
-  // }
-  questions[key] = value;
-  fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
-  res.json(questions);
-});
+// app.put("/questions", (req, res) => {
+//   let key = Object.keys(req.body)[0];
+//   let value = Object.values(req.body)[0];
+//   // if (key in questions) {
+//   //   questions[key] = value;
+//   //   fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
+//   //   res.json(questions);
+//   // }
+//   questions[key] = value;
+//   fs.writeFileSync("./JSONtests/mcqtest.json", JSON.stringify(questions));
+//   res.json(questions);
+// });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
