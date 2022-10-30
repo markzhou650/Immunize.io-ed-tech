febd369195f6e1575b4a6477b25d39564f49c9ba
Chris M <cmok97@gmail.com>
2022-10-29 18:54:49 -0700

    updated iframe link and css

--- client/src/components/iframe/Iframe.js
+++ client/src/components/iframe/Iframe.js
@@ -4,7 +4,7 @@ import '../styles/iframe.css'
 const IframeContainer = () => {
     return (
         <div className="iframe-wrapper">
-            <iframe src='https://school.moodledemo.net/login/index.php'></iframe>
+            <iframe src='http://learning.immunize.io/'></iframe>
         </div>
     )
 }
