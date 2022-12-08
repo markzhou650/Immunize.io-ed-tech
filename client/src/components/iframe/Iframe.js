d096e6244cb51810090a6a9f0bc457f3145011c1
Sumeet <sumeetm273@gmail.com>
2022-12-07 19:19:21 -0800

    iframe can login now weo

--- client/src/components/iframe/Iframe.js
+++ client/src/components/iframe/Iframe.js
@@ -1,7 +1,7 @@
 import '../styles/iframe.css'
 
 
-const IframeContainer = ({src='http://learning.immunize.io/'}) => {
+const IframeContainer = ({src='https://learning.immunize.io/login/index.php'}) => {
     return (
         <div className="iframe-wrapper">
             <iframe src={src}></iframe>
